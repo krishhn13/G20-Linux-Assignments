28ceaf38512fce645a7852bc4d7af6bfec7315c5
Krishan Kumar Sah <149761184+krishhn13@users.noreply.github.com>
2025-01-16 13:05:53 +0530

    Done Lab1
    Question : Use the touch command to create sets of empty practice files to use during this lab. In each set, replace X with the numbers 1 through 6. Create six files with names of the form songX.mp3, snapX.jpg, filmX.avi. Create three subdirectories for organizing your files, and name the subdirectories friends, family, and work. Use a single command to create all three subdirectories at the same time.

---
+++ Lab 1/Lab1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#making three different subdirectories friends family work using single line of code
+mkdir friends family work
+
+
+#making files in the subdirectories using just a single line of code 
+touch friends/songs1.mp3 friends/songs2.mp3 family/familyGetTogether.jpg family/JaiHind.jpg work/DSA.txt work/Linux.txt
+
+#I can provide it executable permissions via chmod +x <name-of-the-file>.sh
+#then run it by ./<name-of-the-file>.sh
+
